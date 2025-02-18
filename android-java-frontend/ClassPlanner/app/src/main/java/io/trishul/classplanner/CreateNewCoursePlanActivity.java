bf8ad3a667b37120e753bf48ec9a7bde7ae48db5
Rishab Manocha <rishab.manocha@outlook.com>
2025-02-17 17:10:01 -0800

    Updated the CreateNewCoursePlan activity to show next and back buttons

--- android-java-frontend/ClassPlanner/app/src/main/java/io/trishul/classplanner/CreateNewCoursePlanActivity.java
+++ android-java-frontend/ClassPlanner/app/src/main/java/io/trishul/classplanner/CreateNewCoursePlanActivity.java
@@ -1,15 +1,28 @@
 package io.trishul.classplanner;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateNewCoursePlanActivity extends AppCompatActivity {
-
+    public static final int MAX_STEPS = 4;
+    private int currentStep = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_course_plan);
+
+        Button backButton = findViewById(R.id.button_create_course_plan_back);
+        backButton.setOnClickListener(v -> {
+            currentStep = Math.max(0, currentStep - 1);
+        });
+
+        Button nextButton = findViewById(R.id.button_create_course_plan_next);
+        nextButton.setOnClickListener(v -> {
+            currentStep = Math.min(2, currentStep + 1);
+        });
     }
 
 }
