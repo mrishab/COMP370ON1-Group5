2268ac455714b0c151e4f4d75e07f30b05bac8b4
Rishab Manocha <rishab.manocha@outlook.com>
2025-03-04 20:16:20 -0800

    Added delete endpoint for users

--- spring-java-backend/src/main/java/io/trishul/classplanner/user/UserController.java
+++ spring-java-backend/src/main/java/io/trishul/classplanner/user/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +47,10 @@ public class UserController {
   @PostMapping("")
   public User createUser(@RequestBody User user) {
     return userRepository.save(user);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteUser(@PathVariable("id") Long id) {
+    userRepository.deleteById(id);
   }
 }
