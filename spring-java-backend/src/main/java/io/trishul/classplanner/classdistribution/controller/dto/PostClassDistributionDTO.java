2fee5ff1d507507ea4e96e9c93e17bad065d0a75
Rishab Manocha <rishab.manocha@outlook.com>
2025-04-08 22:19:12 -0700

    Removed unnecessary ClassDistribution code. Converted it into a simple Enum

--- spring-java-backend/src/main/java/io/trishul/classplanner/classdistribution/controller/dto/PostClassDistributionDTO.java
+++
@@ -1,9 +0,0 @@
-package io.trishul.classplanner.classdistribution.controller.dto;
-
-import lombok.Data;
-
-@Data
-public class PostClassDistributionDTO {
-    private String key;
-    private String input;
-}
--- spring-java-backend/src/main/java/io/trishul/classplanner/classdistribution/model/ClassDistribution.java
+++ spring-java-backend/src/main/java/io/trishul/classplanner/classdistribution/model/ClassDistribution.java
@@ -1,23 +1,6 @@
 package io.trishul.classplanner.classdistribution.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Table(name = "class_distribution")
-@Getter
-@Setter
-public class ClassDistribution {
-
-    @Column(nullable = false, unique = true)
-    private String key;
-
-    @Column(nullable = false)
-    private String input;
-
-    @Column(nullable = true)
-    private boolean archived;
+public enum ClassDistribution {
+    CONCENTRATED,
+    SPARSE
 }
