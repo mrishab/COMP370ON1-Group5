623d7b8ea3cccce04802c30c9d57d09845781aa5
Rishab Manocha <rishab.manocha@outlook.com>
2025-04-08 23:19:35 -0700

    Fixes for UserMapper

--- spring-java-backend/src/main/java/io/trishul/classplanner/user/controller/dto/mapper/UserMapper.java
+++ spring-java-backend/src/main/java/io/trishul/classplanner/user/controller/dto/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package io.trishul.classplanner.user.controller.dto.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import io.trishul.classplanner.user.controller.dto.GetUserDTO;
@@ -11,6 +12,16 @@ import io.trishul.classplanner.user.model.User;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     GetUserDTO toGetDTO(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "archived", constant = "false")
     User toEntity(PostUserDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "archived", ignore = true)
     void updateEntity(@MappingTarget User user, PutUserDTO dto);
 }
--- spring-java-backend/src/main/java/io/trishul/classplanner/user/model/User.java
+++ spring-java-backend/src/main/java/io/trishul/classplanner/user/model/User.java
@@ -11,18 +11,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity(name = "_user")
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class User {
     @Id
     @SequenceGenerator(name = "user_generator", sequenceName = "user_sequence", allocationSize = 1)
